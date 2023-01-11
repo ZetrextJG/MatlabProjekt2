fe0c2f581728452ea858be8818d7cbd2239359ee
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-01-11 02:22:39 +0100

    Last changes

--- examples/inverse.m
+++ examples/inverse.m
@@ -2,7 +2,7 @@ sizes = [5 10 20 40 80 160 500 1000 2000 3000 4000 8000]; % Wilkości macierzy
 
 times = zeros(12, 2);
 errors = zeros(12, 1);
-for i=1:11
+for i=1:12
     s = sizes(1, i);
 %     R = randKdiag(s, 5);
     R = randKdiagC(s, 5);
@@ -12,8 +12,8 @@ for i=1:11
 
     errors(i,1) = norm(matlabInv - myInv) / norm(matlabInv);
 
-    times(i, 1) = timeit(@() inv(randKdiagC(s, 5))) - timeit(@() randKdiagC(s, 5));
-    times(i, 2) = timeit(@() solveByChol(randKdiagC(s, 5), eye(s), 5)) - timeit(@() randKdiagC(s, 5)) - timeit(@() eye(s));
+    times(i, 1) = timeit(@() inv(R));
+    times(i, 2) = timeit(@() solveByChol(R, eye(s), 5));
 
     disp(strcat("Done part ", string(i)));
 end
